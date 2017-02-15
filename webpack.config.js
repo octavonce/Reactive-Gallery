3d068040250009ce85b3e780e3ebc5f90d2384c3
octavonce <octavian.oncescu@gmail.com>
2017-02-15 18:11:20 +0200

    Centered image inside the overlay

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
             test: /\.css$/, 
             loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
         },
-        {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=images/[name].[ext]"}
-        ]
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i, 
+            loader: "file-loader?name=images/[name].[ext]"
+        }]
     }
 };

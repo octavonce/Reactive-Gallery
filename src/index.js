0f8214f4f5e4c06090ef379b829bc4d01917e2b2
octavonce <octavian.oncescu@gmail.com>
2017-02-15 23:09:14 +0200

    Tweaked css

--- src/index.js
+++ src/index.js
@@ -6,16 +6,35 @@ const images = [
     'http://lorempixel.com/400/200/',
     'http://lorempixel.com/400/200/',
     'http://lorempixel.com/400/200/',
+    'http://lorempixel.com/400/400/',
     'http://lorempixel.com/500/300/',
     'http://lorempixel.com/1200/800/',
     'http://lorempixel.com/900/400/',
     'http://lorempixel.com/1920/1080/',
     'http://lorempixel.com/400/400/',
     'http://lorempixel.com/400/400/',
-    'http://lorempixel.com/400/400/',
     'http://lorempixel.com/4280/2100/'
+]
+
+const sameSizeImages = [
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
+    'http://lorempixel.com/800/600/',
 ]
 
 const containerId = 'reactive-gallery';
 
-ReactDOM.render(<Root images={images} containerId={containerId} />, document.getElementById(containerId));
+ReactDOM.render(<Root images={sameSizeImages} containerId={containerId} />, document.getElementById(containerId));

f94d1ace449df8212cc95acd4a0608a320e3fa79
octavonce <octavian.oncescu@gmail.com>
2017-02-21 13:17:48 +0200

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map', // Comment this when building for production
     entry: [
-        'webpack-hot-middleware/client',
+        'webpack-hot-middleware/client', // Comment this too
         './src/index'
     ],
     output: {
@@ -15,11 +15,32 @@ module.exports = {
         library: 'reactiveGallery'
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            comments: false,
+            compress: {
+                unused: true,
+                dead_code: true,
+                warnings: false, 
+                drop_debugger: true,
+                conditionals: true,
+                evaluate: true,
+                drop_console: true,
+                sequences: true,
+                booleans: true,
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
     ],
     module: {
         loaders: [{
             test: /\.js$/,
+            exclude: /node_modules/,
             loaders: ['react-hot-loader', 'babel-loader?presets[]=es2015,presets[]=react'],
             include: path.join(__dirname, 'src')
         },
